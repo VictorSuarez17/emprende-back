2017ab9d080c8c4389ea939674820570172ee59b
VictorSuarez17 <vichormanuel@gmail.com>
2023-11-14 02:10:46 -0600

    Correccion del puerto como parametro

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const { env } = require("process");
 require('dotenv').config();
 
 const requestController = () => {
-  console.log("Recibimos nueva requestttttt")
+  console.log("Recibimos nueva request")
 }
 
 const server = http.createServer(requestController)
 
 const PORT = process.env.PORT
 
-server.listen(4000,() => {console.log("Aplicacion corriendo en el puerto " + PORT)})
+server.listen(PORT,() => {console.log("Aplicacion corriendo en el puerto " + PORT)})
